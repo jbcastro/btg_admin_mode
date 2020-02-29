3eb418ebbab21a099cfc3620653d4ec93f2b8732
jbcastro <jbeckcastro810@southseattle.edu>
2020-02-28 19:57:22 -0800

    added build folder

---
+++ client/build/precache-manifest.e945951eb7c5945211733e7170c81dda.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1ee9f6908ea9cfe5c24d8c0212f361b8",
+    "url": "/index.html"
+  },
+  {
+    "revision": "c016620e6048a5e1bf17",
+    "url": "/static/css/main.0adcfc1b.chunk.css"
+  },
+  {
+    "revision": "0b36b10c2bb74f8b5fb6",
+    "url": "/static/js/2.305905cb.chunk.js"
+  },
+  {
+    "revision": "9b35c1366910c0dfc7b3a2227c1aaa77",
+    "url": "/static/js/2.305905cb.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c016620e6048a5e1bf17",
+    "url": "/static/js/main.cc1edde6.chunk.js"
+  },
+  {
+    "revision": "cda345fb50b7e11c0025",
+    "url": "/static/js/runtime-main.844668cf.js"
+  }
+]);
---
+++ server.js
@@ -0,0 +1,162 @@
+const express = require("express");
+var cors = require("cors");
+
+const app = express();
+app.use(cors());
+const port = process.env.PORT || 5000;
+var wineMethods = require("./models/wineMethods");
+var Wines = require("./models/wine");
+const bodyParser = require("body-parser");
+
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+app.engine("html", require("ejs").renderFile);
+
+app.set("view engine", "html");
+
+// console.log that your server is up and running
+app.listen(port, () => console.log(`Listening on port ${port}`));
+// app.get("/", require("./client/public/index"));
+
+// create a GET route
+// app.get('/express_backend', (req, res) => {
+//   res.send({ express: 'YOUR EXPRESS BACKEND IS CONNECTED TO REACT' });
+// });
+app.get("./client", function(req, res) {
+  res.render("index");
+});
+app.get("/express_backend", (req, res, next) => {
+  wineMethods
+    .getAll()
+    .then(items => {
+      // res.sendFile("home", { wines: JSON.stringify(items) });
+      res.send({ express: items });
+    })
+    .catch(err => {
+      return next(err);
+    });
+});
+
+app.get("/express_backend/get/", (req, res, next) => {
+  wineMethods
+    .getOne(req.query._id)
+    .then(items => {
+      res.send({ express: items });
+    })
+    .catch(err => {
+      return next(err);
+    });
+});
+
+app.get("/express_backend/delete", (req, res, next) => {
+  wineMethods
+    .killOne(req.query._id)
+    .then(items => {
+      res.send({ express: items });
+    })
+    .catch(err => {
+      return next(err);
+    });
+});
+
+app.put("/express_backend/add", (req, res, next) => {
+  Wines.updateOne(
+    {
+      _id: req.body._id
+    },
+    {
+      vinyard: req.body.vinyard,
+      name: req.body.name,
+      grape: req.body.grape,
+
+      grapes: req.body.grapes,
+      year: req.body.year,
+      place: req.body.place,
+      area: req.body.area,
+      country: req.body.country,
+      appellation: req.body.appellation,
+      description: req.body.description,
+
+      funfact: req.body.funfact,
+      price: req.body.price,
+      lastUpdated: Date.now(),
+      mise: req.body.mise,
+
+      color: req.body.color,
+      status: req.body.status,
+      picture: req.body.picture,
+      coravin: req.body.coravin
+    },
+    (err, result) => {
+      if (err) return next(err);
+      res.json({ updated: result.nModified, _id: req.body._id });
+    }
+  );
+});
+
+app.post("/express_backend/add", (req, res, next) => {
+  if (!req.body._id) {
+    let wine = new Wines(
+      // {
+      //   _id: req.body._id
+      // },
+      {
+        vinyard: req.body.vinyard,
+        name: req.body.name,
+        grape: req.body.grape,
+        grapes: req.body.grapes,
+        year: req.body.year,
+        place: req.body.place,
+        area: req.body.area,
+        country: req.body.country,
+        appellation: req.body.appellation,
+        description: req.body.description,
+        funfact: req.body.funfact,
+        timestamp: Date.now(),
+        price: req.body.price,
+        mise: req.body.mise,
+        lastUpdated: Date.now(),
+
+        color: req.body.color,
+        status: req.body.status,
+        picture: req.body.picture,
+        coravin: req.body.coravin
+      }
+    );
+    // wine.save().then(item => res.json(item));
+    wine.save((err, newWine) => {
+      if (err) return next(err);
+      return res.json({ updated: 0, _id: newWine._id });
+    });
+  } else {
+    Wines.updateOne(
+      { _id: req.body._id },
+      {
+        vinyard: req.body.vinyard,
+        name: req.body.name,
+        grape: req.body.grape,
+        grapes: req.body.grapes,
+        year: req.body.year,
+        place: req.body.place,
+        area: req.body.area,
+        country: req.body.country,
+        appellation: req.body.appellation,
+        description: req.body.description,
+        funfact: req.body.funfact,
+        timestamp: Date.now(),
+        price: req.body.price,
+        mise: req.body.mise,
+        lastUpdated: Date.now(),
+
+        color: req.body.color,
+        status: req.body.status,
+        picture: req.body.picture,
+        coravin: req.body.coravin
+      },
+      (err, result) => {
+        if (err) return next(err);
+        res.json({ updated: result.nModified, _id: req.body._id });
+      }
+    );
+  }
+});
