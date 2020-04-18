f11667b50be3e2b2f7e8f6be070da5de7cb39a1d
jbcastro <jbeckcastro810@southseattle.edu>
2020-04-18 16:00:14 -0700

    fixing vulnerabilities

--- models/connection.js
+++ models/connection.js
@@ -1,2 +1,5 @@
 exports.connectionString2 =
+  //demo
   "mongodb://dbuser:dbuserpassword1@ds259787.mlab.com:59787/wines";
+//regular
+// "mongodb://jbcastro:!RN74Winez@ds161804.mlab.com:61804/glasses";
